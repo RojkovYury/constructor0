6364ce9264c989f2d0c9351f4a5484b46f520f31
RojkovYury <rojkov.yury@gmail.com>
2022-09-27 20:42:21 +0300

    first commit

---
+++ src/model.js
@@ -0,0 +1,12 @@
+import image from './assets/image.png'
+
+export const model= [
+    {type: 'title', value: 'hello from JS!'},
+    {type: 'text', value: 'here we go with some text'},
+    {type: 'columns', value: [
+            '11111111111',
+            '22222222222',
+            '33333333333'
+        ]},
+    {type: 'image', value: image}
+]
---
+++ src/templates.js
@@ -0,0 +1,25 @@
+import {row,col} from './utils'
+
+function title(block) {
+    return row(col('<h1>${block.value}</h1>'))
+}
+
+function text(block) {
+    return row(col('<p>${block.value}</p>'))
+}
+
+function columns(block) {
+    const html = block.value.map(item => col(item))
+    return row(html.join(''))
+}
+
+function image(block) {
+    return row('<img crc=$"{block.value}" />')
+}
+
+export const templates = {
+    title,
+    text,
+    image,
+    colums
+}
